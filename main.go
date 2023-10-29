2e711a855f37772397b3a77e95ee308b14a13ec6
paakofiaidoo <paakofiaidoo17@gmail.com>
2023-10-29 21:22:00 +0000

    first rest api
    Signed-off-by: paakofiaidoo <paakofiaidoo17@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	println("hello world")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hellWorld)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+func hellWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "Hello , world!")
 }
